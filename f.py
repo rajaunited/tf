9346ab749a9ccbb4808af2857e1c7da2f24be318
A H A D <93983627+ahadz-git@users.noreply.github.com>
2022-03-29 19:10:24 +0530

    Update f.py

--- f.py
+++ f.py
@@ -6,7 +6,7 @@ APP_ID = 11542235
 API_HASH = "e46bdcd08e7818b6ecc0448446dc7fc3"
 BOT_TOKEN = "5132840587:AAGR_XH1OTqWSVKKre2uBgpaoDrrGAOIT1M"
 
-with Client("pyrogram", api_id=APP_ID, api_hash=API_HASH,bot_token=BOT_TOKEN, no_updates=True) as app:
+with Client("pyrogram", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, no_updates=True) as app:
     for message in app.iter_history(-1001673241127, reverse=True):
         try:
             message.copy(-1001655189003)
