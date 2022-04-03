cbcbbe0b3f30faa6b653045a1410b1fc9fe699a7
A H A D <93983627+ahadz-git@users.noreply.github.com>
2022-04-03 16:50:11 +0530

    Update f.py

--- f.py
+++ f.py
@@ -1,4 +1,13 @@
 import requests
 
-a = requests.get("https://t.me/AHADx1337/3?embed=1").text
-print(a)
+_headers = {
+  'accept-language': 'en-US,en;q=0.9',
+  'user-agent': 'Mozilla/5.0 (Linux; Android 10; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Mobile Safari/537.36',
+  'x-requested-with': 'XMLHttpRequest'
+}
+
+main_url = "https://t.me/AHADx1337/3"
+session = requests.session()
+session.headers.update(_headers)
+main_res = session.get(main_url).text
+print(main_res)
