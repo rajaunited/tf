960517a66ba8e9a5c890b092269fc764845925be
A H A D <93983627+ahadz-git@users.noreply.github.com>
2022-04-03 20:34:45 +0530

    Update f.py

--- f.py
+++ f.py
@@ -1,23 +1,3 @@
-import sys
-from smartproxy.smartproxy import Requests
+import webbrowser
 
-r = Requests.request(request_type='get', url='https://t.me/AHADx1337/3')
-print(r.text)
-
-
-
-
-
-'''
-_headers = {
-  'accept-language': 'en-US,en;q=0.9',
-  'user-agent': 'Mozilla/5.0 (Linux; Android 10; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Mobile Safari/537.36',
-  'x-requested-with': 'XMLHttpRequest'
-}
-
-main_url = "https://t.me/AHADx1337/3"
-session = requests.session()
-session.headers.update(_headers)
-main_res = session.get(main_url).text
-print(main_res)
-'''
+webbrowser.open("https://t.me/hmnok/2", new=0)
