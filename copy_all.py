d27d6f656d32327ce96a53ee2e1a5323758c3dd1
A H A D <93983627+ahadz-git@users.noreply.github.com>
2022-05-18 15:08:41 +0530

    Update copy_all.py

--- copy_all.py
+++ copy_all.py
@@ -4,9 +4,9 @@ from time import sleep
 
 APP_ID = 11542235
 API_HASH = "e46bdcd08e7818b6ecc0448446dc7fc3"
-SESSION = "AQCwHtsARGXii3QomGF7lirWNnbS8TlplcO_x4NGJIUSIxmlrAHnsopt6CQ_y78eOZLrya_oY2doKY0-HzNlKUqAR0LWJmhpkujr7qRSIlnWL6PrTUCIschKoPWV-MStOQ5qU3dUkcQshE2jDG06KjO7doszlVKe6r8dk6q3mZKs7F8rv-BQMUGh8pqfbpJs5TRLEOw0hdzQI8HnzcIuWC2k7Z54HK4ioN8QVJ6XjW8kZHS9VfZ4cOac9hIzYKwbAmwgdiiGpMceU-OZb-X6zKbPPtShBnPum5m8golJLc2O3KAhXyxHQ-pBFQIEBSOTOZE9xGDQW-6VSzlUeLYLF8O2jwfmLAAAAAE6r_0HAA"
+SESSION = "AQAaCgStFBP_a60gMmg4TlUT1OFmqdFcVIhTY_1ANfci5Cc4pPYO9_2FStxhufXij9p2oLYmDteHhobwa13L_9zFcWTLsA2AZ4jhDOrho8wE00h8gO5wDuAzSteTPdw9ITNq6IVOQGeWCyI0q8AavBTsv96Iv30-RVyuwF_ZLRT3zIKsOQ2_ibjONAFPsnq3fzXrUTSJqHRr-DbobJW5V2D4mijrXP6G7KXwtLdbq-ONb9eWYnRoQgAnoXv7Eb18aNfeCFKyYNtDqZaj38Fa_UQlUiN8CPkswD3MXszTNJKcBku71UXRxhIL4ER-p__hP6RUPnuAbDGGBJMJsR3vBwVPAAAAATqv_QcA"
 
-with Client(name='session', api_id=APP_ID, api_hash=API_HASH, session_string=SESSION) as app:
+with Client(SESSION, api_id=APP_ID, api_hash=API_HASH) as app:
     for message in app.iter_history(-1001766664486, reverse=True):
         try:
             message.copy(-1001627190543)
