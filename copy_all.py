9f16aa0c1f6cc2d77b36fb5d3a9b0d9701341abb
rajaunited <85538988+rajaunited@users.noreply.github.com>
2022-05-18 22:24:42 +0530

    Update copy_all.py

--- copy_all.py
+++ copy_all.py
@@ -2,14 +2,14 @@ from time import sleep
 from pyrogram import Client
 from pyrogram.errors import FloodWait
 
-APP_ID = 11542235
-API_HASH = "e46bdcd08e7818b6ecc0448446dc7fc3"
-SESSION = "AQAaCgStFBP_a60gMmg4TlUT1OFmqdFcVIhTY_1ANfci5Cc4pPYO9_2FStxhufXij9p2oLYmDteHhobwa13L_9zFcWTLsA2AZ4jhDOrho8wE00h8gO5wDuAzSteTPdw9ITNq6IVOQGeWCyI0q8AavBTsv96Iv30-RVyuwF_ZLRT3zIKsOQ2_ibjONAFPsnq3fzXrUTSJqHRr-DbobJW5V2D4mijrXP6G7KXwtLdbq-ONb9eWYnRoQgAnoXv7Eb18aNfeCFKyYNtDqZaj38Fa_UQlUiN8CPkswD3MXszTNJKcBku71UXRxhIL4ER-p__hP6RUPnuAbDGGBJMJsR3vBwVPAAAAATqv_QcA"
+APP_ID = 14985580
+API_HASH = "91e12775f7841ca784b463070df7dcbe"
+SESSION = "AQByqgDJnaePmzWdK8ppEioGPfeEW19PsdGlF8Fqt2ljcnPcmnyOT0CuhwH3MSKdXKpqUKFPQBTVPv7NGhggHH3gIsVRUznG_C4O2bOiSDAqJtTdUoYXPhsmW7QVV0Cbs3rpoe5iOShb7o6Uv35txKa1QYbj0hXz4uJQsbY5YvIJRWPsl008eb_ZHZ"
 
 with Client(SESSION, api_id=APP_ID, api_hash=API_HASH) as app:
     for message in app.iter_history(-1001277293346, reverse=True):
         try:
-            message.copy(-1001560716943)
+            message.copy(-1001638881016)
             print(f"Sussesfuly Copied Message Id: {message.message_id}")
             sleep(4)
         except FloodWait as e:
