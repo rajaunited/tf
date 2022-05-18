602e5553e20216dd7d7166d6c5b846d48efe04ea
A H A D <93983627+ahadz-git@users.noreply.github.com>
2022-05-18 18:07:13 +0530

    Update copy_all.py

--- copy_all.py
+++ copy_all.py
@@ -8,7 +8,7 @@ API_HASH = "e46bdcd08e7818b6ecc0448446dc7fc3"
 SESSION = "AQAaCgStFBP_a60gMmg4TlUT1OFmqdFcVIhTY_1ANfci5Cc4pPYO9_2FStxhufXij9p2oLYmDteHhobwa13L_9zFcWTLsA2AZ4jhDOrho8wE00h8gO5wDuAzSteTPdw9ITNq6IVOQGeWCyI0q8AavBTsv96Iv30-RVyuwF_ZLRT3zIKsOQ2_ibjONAFPsnq3fzXrUTSJqHRr-DbobJW5V2D4mijrXP6G7KXwtLdbq-ONb9eWYnRoQgAnoXv7Eb18aNfeCFKyYNtDqZaj38Fa_UQlUiN8CPkswD3MXszTNJKcBku71UXRxhIL4ER-p__hP6RUPnuAbDGGBJMJsR3vBwVPAAAAATqv_QcA"
 
 with Client(SESSION, api_id=APP_ID, api_hash=API_HASH) as app:
-    for message in app.iter_history(-1001277293346, reverse=True):
+    for message in app.iter_history(-1001560716943, reverse=True):
         try:
             message.copy(-1001701282526)
             print(f"Sussesfuly Copied Message Id: {message.message_id}")
